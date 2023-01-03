77958a570c43e402ebae2bf077369bbf0c983dbc
GiladFisher <giladfisher98@gmail.com>
2023-01-03 19:33:51 +0200

    initial commit

---
+++ src/MainFun.java
@@ -0,0 +1,10 @@
+public class MainFun {
+    public static void main(String[] args) {
+        //TaskType taskType;
+//        System.out.println(taskType);
+//        System.out.println(taskType.getPriorityValue());
+//        taskType.setPriority(5);
+//        System.out.println(taskType.getPriorityValue());
+//        System.out.println(taskType.getType());
+    }
+}
