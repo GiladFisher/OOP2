9c92c0b90fbd3e2f542ed6ca5e118d12ffccad52
GiladFisher <giladfisher98@gmail.com>
2023-01-04 17:44:35 +0200

    Added documentations

--- src/MainFun.java
+++ src/MainFun.java
@@ -2,7 +2,9 @@ import java.util.concurrent.Callable;
 
 public class MainFun {
     public static void main(String[] args) throws Exception {
-        Callable<Integer> call = () -> {return 7; };
+        Callable<Integer> call = () -> {
+            return 7;
+        };
 
         Task<Integer> task = new Task<>(TaskType.COMPUTATIONAL, call);
         task.setPriority(5);
--- src/Task.java
+++ src/Task.java
@@ -13,32 +13,37 @@ public class Task<T> implements Comparable<T>, Callable<T> {
     public void setTask(Callable<T> task){
         this.task = task;
     }
+
+    // factory method that creates a task using only a callable object and a priority
     public static Task createTask(Callable task, int priority){
         TaskType taskType = TaskType.OTHER;
         try{
             taskType.setPriority(priority);
-        } catch (IllegalArgumentException e){
+        }
+        catch (IllegalArgumentException e){
             System.out.println(e.getMessage());
+            return null;
         }
         return new Task(taskType, task);
     }
-
-
     @Override
     public int compareTo(T o) {  // by taskTypePriority
         return this.taskType.getPriorityValue() - ((Task<T>)o).taskType.getPriorityValue();
     }
 
-    public TaskType getTaskType(){
+    public TaskType getTaskType(){ // return taskType
         return taskType;
     }
-    public int getPriority(){
+    public int getPriority(){ // return taskTypePriority
         return taskType.getPriorityValue();
     }
-    public void setPriority(int priority){
-        taskType.setPriority(priority);
+    public void setPriority(int priority){ // set taskTypePriority
+        try {
+            taskType.setPriority(priority);
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
     }
-
     @Override
     public T call() throws Exception {
         return task.call();
